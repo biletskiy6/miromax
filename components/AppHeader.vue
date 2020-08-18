88a993fadbf6478ad3a486e62ac88b5705520408
Victor Biletskiy <victorbiletskiy82@gmail.com>
2020-08-18 18:25:45 +0300

    initial commit

---
+++ components/AppHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <header class="app-header">
+    <div class="app-header__left">
+      <button class="menu-toggle app-header-button">
+        <i class="material-icons">menu</i>
+      </button>
+
+      <a href="#" class="logo">
+        <img src="logo.png" alt="Miromax Logo" />
+      </a>
+    </div>
+    <div class="app-header__right">
+      <ul class="header-meta">
+        <li class="header-meta__item schedule">
+          <h6>Сегодня, 17 серпня</h6>
+          <button class="app-header-button">
+            <i class="material-icons">date_range</i>
+          </button>
+        </li>
+        <li class="header-meta__item movie-session">
+          <h6>Расписание сеаносв</h6>
+          <button class="app-header-button">
+            <i class="material-icons">drag_indicator</i>
+          </button>
+        </li>
+        <li class="header-meta__item filters">
+          <h6>Расписание сеаносв</h6>
+          <button class="app-header-button">
+            <i class="material-icons">filter_list</i>
+          </button>
+        </li>
+        <li class="header-meta__item user-profile">
+          <h6>Расписание сеаносв</h6>
+          <button class="app-header-button">
+            <i class="material-icons">person</i>
+          </button>
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <AppHeader />
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader";
+
+export default {
+  components: {
+    AppHeader,
+  },
+};
+</script>
